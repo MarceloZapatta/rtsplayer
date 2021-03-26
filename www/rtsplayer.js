bbd274262e93262701835da95caed282d1e3f9ad
Marcelo <marcelozapatta0@gmail.com>
2021-03-25 23:43:18 -0300

    feat: exporting module

--- www/rtsplayer.js
+++ www/rtsplayer.js
@@ -1,9 +1,10 @@
-var exec = require('cordova/exec');
+var exec = require("cordova/exec");
 
-exports.watchVideo = function(moviePath, success, error) {
+module.exports = {
+  watchVideo: function (moviePath, success, error) {
     exec(success, error, "rtsplayer", "watchVideo", [moviePath]);
-};
-
-exports.watch = function(moviePath, user, password, success, error) {
-	exec(success, error, "rtsplayer", "watch", [moviePath, user, password]);
+  },
+  watch: function (moviePath, user, password, success, error) {
+    exec(success, error, "rtsplayer", "watch", [moviePath, user, password]);
+  },
 };
